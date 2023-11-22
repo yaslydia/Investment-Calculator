b16c48b62104a0052fd68ebdcd4281a6d593f715
xuanchen <98302650+yaslydia@users.noreply.github.com>
2023-11-21 20:54:47 -0800

    Enabled modifying userInput

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import logo from "../assets/investment-calculator-logo.png";
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="" />
+    <header id="header">
+      <img src={logo} alt="Logo" />
       <h1>Investment Calculator</h1>
     </header>
   );
